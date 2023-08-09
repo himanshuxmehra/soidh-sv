9406de0fb23a9298e16553f17e986765e9162369
himanshuxmehra <himanshu24mehra@gmail.com>
2023-08-09 09:26:26 +0530

    Made endpoint compliment with app

--- app.ts
+++ app.ts
@@ -23,37 +23,55 @@ const storage = multer.diskStorage({
   }
 })
 
-const upload = multer({storage: storage}).single('images')
-
+const upload = multer({storage: storage}).single('image')
+// const upload = multer({ storage });
 // app.use(cors());
 // app.use(express.json);
 app.use(express.urlencoded({ extended: false }));
 
 
 
-app.post("/upload", (req,res)=>{
-  //console.log(req.body);
-  upload(req, res, function (err) {
-    if (err instanceof multer.MulterError) {
-      // A Multer error occurred when uploading.
-      console.log("A Multer error occurred when uploading: ", err)
-      return res.status(400).send({
-        message: 'A Multer error occurred when uploading'
-     });
-    } else if (err) {
-      // An unknown error occurred when uploading.
-      console.log("An unknown error occurred when uploading: ", err);
-      return res.status(400).send({
-        message: 'An unknown error occurred when uploading'
-     });
-    }
-  });
-  //console.log(req.file);
+// app.post("/upload", (req,res)=>{
+//   //console.log(req.body);
+//   upload(req, res, function (err) {
+//     if (err instanceof multer.MulterError) {
+//       // A Multer error occurred when uploading.
+//       console.log("A Multer error occurred when uploading: ", err)
+//       return res.status(400).send({
+//         message: 'A Multer error occurred when uploading'
+//      });
+//     } else if (err) {
+//       // An unknown error occurred when uploading.
+//       console.log("An unknown error occurred when uploading: ", err);
+//       return res.status(400).send({
+//         message: 'An unknown error occurred when uploading'
+//      });
+//     }
+//   });
+//   //console.log(req.file);
 
-  return res.status(200).send({
-    message: 'File uploaded!'
- });
+//   return res.status(200).send({
+//     message: 'File uploaded!'
+//  });
+// });
+app.post('/upload', upload, (req, res) => {
+  if (!req.file) {
+    return res.status(400).json({ error: 'No image provided' });
+  }
+
+  // Process the image (e.g., save it to a database or storage service)
+  // Here, we'll just send back a success response with the image details
+  // console.log(req)
+  const image = req.file;
+  // console.log(req.body)
+  res.status(200).json({
+    message: 'Image uploaded successfully',
+    originalname: image.originalname,
+    mimetype: image.mimetype,
+    size: image.size,
+  });
 });
+
 
 app.post('/createFolder', (req,res)=>{
   console.log(req.body)
