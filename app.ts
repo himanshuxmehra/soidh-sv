54ee4177d00df04a3289185c290883ed754fa07b
himanshuxmehra <himanshu24mehra@gmail.com>
2023-08-29 22:15:11 +0530

    changes to endpoints

--- app.ts
+++ app.ts
@@ -1,7 +1,8 @@
-import cors from 'cors';
-import multer from 'multer';
-import fs from 'fs';
-import express from 'express';
+import cors from "cors";
+import multer from "multer";
+import fs from "fs";
+import express from "express";
+
 
 const app = express();
 
@@ -11,25 +12,23 @@ declare global {
 global.__basedir = __dirname;
 
 const storage = multer.diskStorage({
-  destination: function(req, file, cb){
-    console.log(req.body)
-    const { userId,folderId } = req.body;
+  destination: function (req, file, cb) {
+    console.log(req.body);
+    const { userId, folderId } = req.body;
     const path = `./uploads/${userId}/${folderId}`;
     fs.mkdirSync(path, { recursive: true });
-    cb (null, path);
+    cb(null, path);
   },
-  filename: function(req, file, cb){
-    cb (null, `${Date.now()}-${file.originalname}`);
-  }
-})
+  filename: function (req, file, cb) {
+    cb(null, `${Date.now()}-${file.originalname}`);
+  },
+});
 
-const upload = multer({storage: storage}).single('image')
+const upload = multer({ storage: storage }).single("image");
 // const upload = multer({ storage });
 // app.use(cors());
 // app.use(express.json);
 app.use(express.urlencoded({ extended: false }));
-
-
 
 // app.post("/upload", (req,res)=>{
 //   //console.log(req.body);
@@ -54,9 +53,9 @@ app.use(express.urlencoded({ extended: false }));
 //     message: 'File uploaded!'
 //  });
 // });
-app.post('/upload', upload, (req, res) => {
+app.post("/upload", upload, (req, res) => {
   if (!req.file) {
-    return res.status(400).json({ error: 'No image provided' });
+    return res.status(400).json({ error: "No image provided" });
   }
 
   // Process the image (e.g., save it to a database or storage service)
@@ -64,27 +63,83 @@ app.post('/upload', upload, (req, res) => {
   // console.log(req)
   const image = req.file;
   // console.log(req.body)
+
   res.status(200).json({
-    message: 'Image uploaded successfully',
+    message: "Image uploaded successfully",
     originalname: image.originalname,
     mimetype: image.mimetype,
     size: image.size,
   });
 });
 
+app.get("/isUploaded", async (req, res) => {
+  try {
+    const rawData = await fs.readFile('data.json', 'utf-8');
+    const jsonData = JSON.parse(rawData);
 
-app.post('/createFolder', (req,res)=>{
-  console.log(req.body)
-  const { userId,folderId } = req.body;
+    // Get the value from the query parameter
+    const searchValue = req.body.parameter as string;
+
+    // Filter the data based on the searchValue
+    const filteredData = jsonData.filter((item: any) =>
+      item.name.toLowerCase().includes(searchValue.toLowerCase())
+    );
+
+    if (filteredData.length > 0) {
+      res.status(200).json({ message: 'Value found', data: filteredData });
+    } else {
+      res.status(404).json({ message: 'Value not found' });
+    }
+  } catch (error) {
+    console.error('Error reading data:', error);
+    res.status(500).json({ error: 'Failed to read data' });
+  }
+});
+
+app.post("/isUploaded", async (req, res) => {
+  const { filename, value } = req.body;
+
+  if (!filename || !value) {
+    return res.status(400).json({ error: "Both key and value are required" });
+  }
+
+  try {
+    const rawData = await fs.readFile("data.json", "utf-8");
+    const jsonData = JSON.parse(rawData);
+
+    // Add the new key-value pair to the data
+    jsonData.push({ [filename]: value });
+
+    // Write the updated data back to the file
+    await fs.writeFile("data.json", JSON.stringify(jsonData, null, 2));
+
+    res.json({ message: "added successfully" });
+  } catch (error) {
+    console.error("Error adding key-value pair:", error);
+    res.status(500).json({ error: "Failed to add key-value pair" });
+  }
+});
+
+app.post("/createFolder", (req, res) => {
+  console.log(req.body);
+  const { userId, folderId } = req.body;
   const path = `./uploads/${userId}/${folderId}`;
   fs.mkdirSync(path, { recursive: true });
   return res.status(200).send({
-    message: 'File uploaded!'
- });
-})
+    message: "File uploaded!",
+  });
+});
+
+app.post("/getFolders", (req, res) => {
+  console.log(req.body);
+  const { userId } = req.body;
+  return res.status(200).send({
+    message: "File uploaded!",
+  });
+});
 
 let port = 8080;
 
 app.listen(port, () => {
   console.log(`Running at localhost:${port}`);
-})
+});
